55387e1c51190ae2e7e4c99a413098393cd231a3
Sourav11gcetts <souravkamilya555@gmail.com>
2023-07-08 17:15:34 +0530

    Navbar and Bootstrap added

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,15 +19,18 @@ import { Component } from '@angular/core';
 export class ListTodosComponent {
 
   todos = [
-    {id:1, description:'Learn DSA'},
-    {id:2, description:'Learn Angular'},
-    {id:3, description:'Learn Spring Boot Well'},
-    {id:4, description:'Learn Other Spring Modules'}
+    new Todo(1,'Learn DSA', false, new Date()),
+    new Todo(2,'Fullstack Development', false, new Date()),
+    new Todo(3,'Recursion', false, new Date()),
+    
+    // {id:2, description:'Learn Angular'},
+    // {id:3, description:'Learn Spring Boot Well'},
+    // {id:4, description:'Learn Other Spring Modules'}
   ]
 
-  todo = {
-    // id : 1,
-    // description : 'Learn Recursion'
-  }
+  // todo = {
+  //   id : 1,
+  //   description : 'Learn Recursion'
+  // }
 
 }
